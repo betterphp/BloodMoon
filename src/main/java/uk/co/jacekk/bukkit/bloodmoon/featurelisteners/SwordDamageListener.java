08c59fe49b1dba82c8f381163c52475f4cb2a4e1
Jacek <jacek@kuzemczak.co.uk>
2012-12-14 03:02:23 +0000

    Make sure we only modify sword damages (closes #30)

--- src/main/java/uk/co/jacekk/bukkit/bloodmoon/featurelisteners/SwordDamageListener.java
+++ src/main/java/uk/co/jacekk/bukkit/bloodmoon/featurelisteners/SwordDamageListener.java
@@ -33,13 +33,15 @@ public class SwordDamageListener extends BaseListener<BloodMoon> {
 		if (event.getCause() == DamageCause.ENTITY_ATTACK && plugin.isActive(entity.getWorld().getName())){
 			if (entity instanceof Creature){
 				Creature creature = (Creature) entity;
+				String creatureName = creature.getType().name().toUpperCase();
 				LivingEntity target = creature.getTarget();
 				
 				if (target instanceof Player){
 					Player player = (Player) target;
 					ItemStack item = player.getItemInHand();
+					String itemName = item.getType().name().toUpperCase();
 					
-					if (plugin.config.getStringList(Config.FEATURE_SPAWN_ON_KILL_MOBS).contains(creature.getType().name().toUpperCase()) && this.rand.nextInt(100) <= plugin.config.getInt(Config.FEATURE_SWORD_DAMAGE_CHANCE)){
+					if (plugin.config.getStringList(Config.FEATURE_SPAWN_ON_KILL_MOBS).contains(creatureName) && itemName.endsWith("_SWORD") && this.rand.nextInt(100) <= plugin.config.getInt(Config.FEATURE_SWORD_DAMAGE_CHANCE)){
 						short damage = item.getDurability();
 						short remove = (short) (item.getType().getMaxDurability() / 50);
 						
