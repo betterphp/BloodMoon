667dec7c903ddd71d0e89d8a86063fd52586ee74
betterphp <jacek@kuzemczak.co.uk>
2013-01-01 16:15:47 +0000

    Fixed creepers being able to explode in protected regions (closes #43)

--- src/main/java/uk/co/jacekk/bukkit/bloodmoon/feature/SuperCreepersListener.java
+++ src/main/java/uk/co/jacekk/bukkit/bloodmoon/feature/SuperCreepersListener.java
@@ -1,11 +1,9 @@
 package uk.co.jacekk.bukkit.bloodmoon.feature;
 
-import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
-import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.ExplosionPrimeEvent;
 
 import uk.co.jacekk.bukkit.baseplugin.v7.config.PluginConfig;
 import uk.co.jacekk.bukkit.baseplugin.v7.event.BaseListener;
@@ -18,18 +16,15 @@ public class SuperCreepersListener extends BaseListener<BloodMoon> {
 		super(plugin);
 	}
 	
-	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-	public void onEntityExplode(EntityExplodeEvent event){
-		if (event.getEntity() != null && event.getEntityType() == EntityType.CREEPER && !event.blockList().isEmpty()){
-			Location location = event.getLocation();
-			World world = location.getWorld();
-			String worldName = world.getName();
+	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+	public void onExplosionProme(ExplosionPrimeEvent event){
+		if (event.getEntity() != null && event.getEntityType() == EntityType.CREEPER){
+			String worldName = event.getEntity().getWorld().getName();
 			PluginConfig worldConfig = plugin.getConfig(worldName);
 			
 			if (plugin.isActive(worldName) && worldConfig.getBoolean(Config.FEATURE_SUPER_CREEPERS_ENABLED)){
-				event.setCancelled(true);
-				
-				world.createExplosion(location, (float) worldConfig.getDouble(Config.FEATURE_SUPER_CREEPERS_POWER), worldConfig.getBoolean(Config.FEATURE_SUPER_CREEPERS_FIRE));
+				event.setRadius((float) worldConfig.getDouble(Config.FEATURE_SUPER_CREEPERS_POWER));
+				event.setFire(worldConfig.getBoolean(Config.FEATURE_SUPER_CREEPERS_FIRE));
 			}
 		}
 	}
