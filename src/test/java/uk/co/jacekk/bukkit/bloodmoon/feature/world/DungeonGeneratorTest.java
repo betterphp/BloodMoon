ec25deb2b131f1b26876a835198d11ae48d001e7
betterphp <jacek@kuzemczak.co.uk>
2013-04-04 14:14:23 +0100

    Added a test for the field names used by the dungeon generator.

---
+++ src/test/java/uk/co/jacekk/bukkit/bloodmoon/feature/world/DungeonGeneratorTest.java
@@ -0,0 +1,21 @@
+package uk.co.jacekk.bukkit.bloodmoon.feature.world;
+
+import net.minecraft.server.v1_5_R2.MobSpawnerAbstract;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DungeonGeneratorTest {
+	
+	@Test
+	public void testReflection(){
+		try{
+			for (String name : new String[]{"minSpawnDelay", "maxSpawnDelay", "spawnCount", "maxNearbyEntities", "spawnRange"}){
+				MobSpawnerAbstract.class.getDeclaredField(name);
+			}
+		}catch (Exception e){
+			Assert.fail(e.getMessage());
+		}
+	}
+	
+}
