087ba5fe5070cd6c45795199f6efd402b2fb6518
Jacek <jacek@kuzemczak.co.uk>
2013-07-16 02:57:10 +0100

    Use the correct config

--- src/main/java/uk/co/jacekk/bukkit/bloodmoon/entity/BloodMoonEntityGiantZombie.java
+++ src/main/java/uk/co/jacekk/bukkit/bloodmoon/entity/BloodMoonEntityGiantZombie.java
@@ -8,6 +8,7 @@ import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.craftbukkit.v1_6_R2.entity.CraftLivingEntity;
 
+import uk.co.jacekk.bukkit.baseplugin.config.PluginConfig;
 import uk.co.jacekk.bukkit.bloodmoon.BloodMoon;
 import uk.co.jacekk.bukkit.bloodmoon.Config;
 
@@ -21,9 +22,10 @@ public class BloodMoonEntityGiantZombie extends BloodMoonEntityMonster {
 	public void onTick(){
 		if (this.nmsEntity.locX != this.nmsEntity.lastX || this.nmsEntity.locZ != this.nmsEntity.lastZ){
 			World world = this.bukkitEntity.getWorld();
+			PluginConfig worldConfig = plugin.getConfig(world.getName());
 			Block block = world.getBlockAt((int) this.nmsEntity.locX, (int) this.nmsEntity.locY - 1, (int) this.nmsEntity.locZ);
 			
-			if (plugin.config.getStringList(Config.FEATURE_GIANTS_BREAK_BLOCKS).contains(block.getType().name())){
+			if (worldConfig.getStringList(Config.FEATURE_GIANTS_BREAK_BLOCKS).contains(block.getType().name())){
 				block.setType(Material.AIR);
 				world.playSound(this.bukkitEntity.getLocation(), Sound.ZOMBIE_WOODBREAK, 1.0f, 1.0f);
 			}
