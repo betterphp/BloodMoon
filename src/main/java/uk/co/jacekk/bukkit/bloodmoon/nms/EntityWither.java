ad425b528b635615c0ecde5c94e093497b63a01c
khomitchr <roman_khomitch@hotmail.com>
2014-12-03 23:48:44 -1000

    Rename to 1.8

--- src/main/java/uk/co/jacekk/bukkit/bloodmoon/nms/EntityWither.java
+++ src/main/java/uk/co/jacekk/bukkit/bloodmoon/nms/EntityWither.java
@@ -1,18 +1,18 @@
 package uk.co.jacekk.bukkit.bloodmoon.nms;
 
-import net.minecraft.server.v1_7_R4.World;
+import net.minecraft.server.v1_8_R1.World;
 
 import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.v1_7_R4.CraftServer;
-import org.bukkit.craftbukkit.v1_7_R4.entity.CraftLivingEntity;
-import org.bukkit.craftbukkit.v1_7_R4.entity.CraftWither;
+import org.bukkit.craftbukkit.v1_8_R1.CraftServer;
+import org.bukkit.craftbukkit.v1_8_R1.entity.CraftLivingEntity;
+import org.bukkit.craftbukkit.v1_8_R1.entity.CraftWither;
 import org.bukkit.plugin.Plugin;
 
 import uk.co.jacekk.bukkit.bloodmoon.BloodMoon;
 import uk.co.jacekk.bukkit.bloodmoon.entity.BloodMoonEntityType;
 import uk.co.jacekk.bukkit.bloodmoon.entity.BloodMoonEntityWither;
 
-public class EntityWither extends net.minecraft.server.v1_7_R4.EntityWither {
+public class EntityWither extends net.minecraft.server.v1_8_R1.EntityWither {
 	
 	private BloodMoon plugin;
 	private BloodMoonEntityWither bloodMoonEntity;
