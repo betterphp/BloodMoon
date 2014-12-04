9d5b7115632d895d40c89778bf9dae750bd24119
khomitchr <roman_khomitch@hotmail.com>
2014-12-03 23:40:35 -1000

    Rename to 1.8

--- src/main/java/uk/co/jacekk/bukkit/bloodmoon/nms/ChunkProviderServer.java
+++ src/main/java/uk/co/jacekk/bukkit/bloodmoon/nms/ChunkProviderServer.java
@@ -3,12 +3,12 @@ package uk.co.jacekk.bukkit.bloodmoon.nms;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.minecraft.server.v1_7_R4.BiomeMeta;
-import net.minecraft.server.v1_7_R4.EntityTypes;
-import net.minecraft.server.v1_7_R4.EnumCreatureType;
-import net.minecraft.server.v1_7_R4.IChunkLoader;
-import net.minecraft.server.v1_7_R4.IChunkProvider;
-import net.minecraft.server.v1_7_R4.WorldServer;
+import net.minecraft.server.v1_8_R1.BiomeMeta;
+import net.minecraft.server.v1_8_R1.EntityTypes;
+import net.minecraft.server.v1_8_R1.EnumCreatureType;
+import net.minecraft.server.v1_8_R1.IChunkLoader;
+import net.minecraft.server.v1_8_R1.IChunkProvider;
+import net.minecraft.server.v1_8_R1.WorldServer;
 
 import org.bukkit.entity.EntityType;
 
