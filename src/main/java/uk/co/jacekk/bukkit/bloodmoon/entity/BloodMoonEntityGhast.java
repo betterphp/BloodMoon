7accdce40ad6a0ae2f0c2e3290fc06d23e41c5b7
khomitchr <roman_khomitch@hotmail.com>
2014-12-03 23:27:07 -1000

    Rename to 1.8

--- src/main/java/uk/co/jacekk/bukkit/bloodmoon/entity/BloodMoonEntityGhast.java
+++ src/main/java/uk/co/jacekk/bukkit/bloodmoon/entity/BloodMoonEntityGhast.java
@@ -1,8 +1,8 @@
 package uk.co.jacekk.bukkit.bloodmoon.entity;
 
-import net.minecraft.server.v1_7_R4.EntityLiving;
+import net.minecraft.server.v1_8_R1.EntityLiving;
 
-import org.bukkit.craftbukkit.v1_7_R4.entity.CraftLivingEntity;
+import org.bukkit.craftbukkit.v1_8_R1.entity.CraftLivingEntity;
 
 import uk.co.jacekk.bukkit.bloodmoon.BloodMoon;
 
